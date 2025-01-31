0d80fe5923132fd494f6823944f5d1f23329e77c
AshutoshRajbhar <ashutoshrajbhar@gmail.com>
2025-01-31 17:21:46 +0530

    Data types added

---
+++ 6_Data_Types.php
@@ -0,0 +1,45 @@
+<!-- Datatypes in PHP  -->
+
+<?php 
+
+/**
+ * There are 8 type of datatype in PHP
+ * 
+ *  String
+ *  Integer
+ *  float
+ *  Boolean
+ *  NUll
+ *  Array
+ *  Object
+ *  Recourse
+ */
+
+?>
+
+<?php 
+$_name = "Jhon Lark";
+echo $_name;
+echo var_dump($_name);
+echo "<br/>";
+echo "<br/>";
+$_number = 10;
+echo $_number;
+echo var_dump($_number);
+echo "<br/>";
+echo "<br/>";
+$_pi = 3.14;
+echo $_pi;
+echo var_dump($_pi);
+echo "<br/>";
+echo "<br/>";
+$_guess = true;
+echo var_dump($_guess);
+?>
+
+<?php 
+$_array = ["Ashutosh", 30, "Single", "Software Developer"];
+echo "$_array";
+echo var_dump($_array);
+?>
+
