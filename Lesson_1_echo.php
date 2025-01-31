a50cf008c6192a66a9736566b5041a63e113efb8
AshutoshRajbhar <ashutoshrajbhar@gmail.com>
2025-01-31 12:03:56 +0530

    Variables lesson added

---
+++ Lesson_1_echo.php
@@ -0,0 +1,108 @@
+<!-- ************************************************************* -->
+
+    <!-- Printing "Hello Word"  -->
+     
+<!-- ************************************************************* -->
+
+
+<?php 
+// for Pring "Hello Word" echo is used.
+
+/*
+Below is the example we can check from there,
+How we can use 'echo'.
+What are the properties of the echo.
+*/
+
+/*
+    1. echo is used to print(Display), whatever we want to display on the screen.
+    2. echo is a 'Language Construct'
+    3. echo is Case Insensitive
+*/
+?>
+
+<!-- ************************************************************* -->
+
+    <!-- LET's Start -->
+
+<!-- ************************************************************* -->
+
+<?php
+echo "Hello World";
+?>
+
+<!-- 
+
+O/P ==> Hello World 
+
+-->
+
+<!-- ************************************************************* -->
+
+    <!-- Case Insensitive -->
+
+<!-- ************************************************************* -->
+
+<?php 
+Echo "Hello World";
+ECHO "Why Say 'Hello World', Just Say 'Hi World'"
+?>
+
+
+<!-- 
+
+O/P ==> Hello World 
+
+O/P ==> Hello WorldWhy Say 'Hello World', Just Say 'Hi World'
+
+-->
+
+
+<!-- ************************************************************* -->
+
+    <!-- Adding Break in the echo -->
+    <!-- for adding break we can use <br/> -->
+
+<!-- ************************************************************* -->
+
+
+<?php
+echo "hello world <br/> Hi World"
+?>
+
+
+<!-- 
+
+
+O/P ==> hello world
+        Hi World
+
+-->
+
+<!-- ************************************************************* -->
+
+    <!-- We Can also do arithematic operations using php -->
+
+<!-- ************************************************************* -->
+
+<?php
+echo 2+10;
+?>
+
+<!-- 
+
+
+O/P ==> 12
+
+-->
+
+<?php
+echo 20*15;
+?>
+
+<!-- 
+
+
+O/P ==> 300
+
+-->
---
+++ Lesson_2_Variables.php
@@ -0,0 +1,8 @@
+<!-- Introduction to Variables in PHP -->
+
+<?php
+/**
+ * Variables are the storage location to store data
+ * 
+ */
+?>
