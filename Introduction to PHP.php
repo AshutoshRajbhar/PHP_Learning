e52938ca0621e2611ba722a1792a5524fe12e4b2
AshutoshRajbhar <ashutoshrajbhar@gmail.com>
2025-01-31 11:11:41 +0530

    Folder and Files  organised

--- Introduction to PHP.php
+++ Introduction to PHP.php
@@ -1,8 +1,35 @@
+<!--  Introduction to PHP -->
+
+<?php
+// php stands for 'Hypertext Preprocessor'.
+// PHP is Server Side Scripting language.
+?>
 
 
+<!-- ************************************************************* -->
+    <!--  COMMENTS in PHP -->
+<!-- ************************************************************* -->
+
+<?php
+
+// Single line comment
+
+/*
+Multiline Comment
+*/
+
+?>
 
 
+<!-- ************************************************************* -->
+    <!-- Tag  -->
+<!-- ************************************************************* -->
 
+
+<?php  // Opeaning Tag
+
+// Closing Tag
+?>
 
 
 <?php 
