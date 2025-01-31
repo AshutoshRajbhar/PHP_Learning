93c4de9f9df51abac33415d715c73b8995536766
AshutoshRajbhar <ashutoshrajbhar@gmail.com>
2025-01-31 18:46:33 +0530

    Conditions Added

---
+++ Conditions_in_php.php
@@ -0,0 +1,31 @@
+<!-- Conditions in PHP  -->
+
+<!-- Types of Conditional Statement  -->
+
+<?php 
+/**
+ 
+ * If Condition
+ * If Else Condition
+ * Else If condition
+ * Switch Statement
+ 
+ **/
+?>
+
+
+<!-- ************************************************************* -->
+    <!-- IF condition  -->
+<!-- ************************************************************* -->
+
+<!-- Syntax  -->
+
+
+<!-- 
+<?php 
+if(condition){
+    // Code goes here
+}
+?> -->
+ 
+
