b15f8bb5d5e53e9f494d8ac71fa763c4a82ec0ae
AshutoshRajbhar <ashutoshrajbhar@gmail.com>
2025-02-03 11:44:31 +0530

    revision completed with increment and decrement operator

--- Resvision.php
+++ Resvision.php
@@ -3,5 +3,21 @@
 <?php 
     $_num1 = 10;
     $_num2 = 15;
-    echo "the Sum of 10 + 15 =" $_num1 + $_num2;
+    echo "the Sum of 10 + 15 =" .$_num1 + $_num2;
 ?>  
+
+<!-- Increment and decrement operator  -->
+
+<?php 
+echo "<br/> <br/> <br/> <br/>";
+?>
+
+<?php 
+$_num3 = 20;
+$_updatedValue = ++$_num3;
+echo "++ preIncrement Operator = ".$_updatedValue;
+// echo $_updatedValue;
+echo "<br/> <br/> <br/>";
+$_newUpdatedValue = --$_num3;
+echo "-- preDecrement Operator = ".$_newUpdatedValue;
+?>
