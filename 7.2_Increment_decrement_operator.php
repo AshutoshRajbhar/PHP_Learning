2a809d330b1f6feede55e9edd108c68a2d67f9a7
AshutoshRajbhar <ashutoshrajbhar@gmail.com>
2025-02-03 17:05:21 +0530

    Operations updated

--- 7.2_Increment_decrement_operator.php
+++ 7.2_Increment_decrement_operator.php
@@ -1,2 +1,20 @@
 <!-- Incrementing/Decrementing Operators  -->
 
+<!-- Pre-Increment  -->
+
+<?php 
+$_a = 2;
+$_b = ++$_a;
+echo $_a ."<br/>";
+echo $_b;
+
+var_dump($_a);
+var_dump($_b);
+?>
+
+<?php 
+$_one = ($_two = 5) + 10;
+
+echo $_one .PHP_EOL;
+echo $_two
+?>
