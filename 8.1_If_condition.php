790025f5cd1aafe04101cb69bce8b25cae98e639
AshutoshRajbhar <ashutoshrajbhar@gmail.com>
2025-02-04 10:24:35 +0530

    If and If else condition added

---
+++ 8.1_If_condition.php
@@ -0,0 +1,8 @@
+<!-- If Condition  -->
+
+<?php 
+$_age =10;
+if($_age < 18 ){
+    echo "<h1>You are not alowed to Drive</h1>";
+}
+?>
---
+++ 8.2_If_Else_condition.php
@@ -0,0 +1,11 @@
+<!-- If Else Condition  -->
+
+<?php 
+$_age = 17;
+if($_age <= 18){
+    echo "you are allowed to drive";
+
+}else{
+    echo "you are not allowed to drive";
+}
+?>
