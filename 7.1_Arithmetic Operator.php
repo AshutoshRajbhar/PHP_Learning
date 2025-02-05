164fccb1069ee4b51052fa41e0363126823d5dfb
AshutoshRajbhar <ashutoshrajbhar@gmail.com>
2025-02-05 18:07:50 +0530

    Arithmetic Operations Updated

--- 7.1_Arithmetic Operator.php
+++ 7.1_Arithmetic Operator.php
@@ -7,10 +7,10 @@ var_dump($_a)
 
 
 <?php 
-$_a + $_b  // Addition
-$_a - $_b  // Substraction
-$_a * $_b   // Multiplication
-$_a / $_b   // Division
-$_a % $_b   // Modulo
-$_a ** $_b  // Exponential
+$_a + $_b;  // Addition
+$_a - $_b;  // Substraction
+$_a * $_b;   // Multiplication
+$_a / $_b;   // Division
+$_a % $_b;   // Modulo
+$_a ** $_b;  // Exponential
 ?>
