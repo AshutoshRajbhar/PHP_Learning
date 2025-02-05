97f8380139e3268878bc66592a143ecab6fe7825
AshutoshRajbhar <ashutoshrajbhar@gmail.com>
2025-02-05 17:04:56 +0530

    Arithmetic Operator and Operator Presedence Updated

--- 7.0_Operator_Precedence.php
+++ 7.0_Operator_Precedence.php
@@ -5,3 +5,9 @@
 Before proceeding further first understand what is Precedence mean  
 
 -->
+
+<?php 
+    echo 1+2-4+5;
+    echo "<br/>";
+    echo 1*8*2;
+?>
--- 7.1_Arithmetic Operator.php
+++ 7.1_Arithmetic Operator.php
@@ -4,3 +4,13 @@
 $_a = 2;
 var_dump($_a)
 ?>
+
+
+<?php 
+$_a + $_b  // Addition
+$_a - $_b  // Substraction
+$_a * $_b   // Multiplication
+$_a / $_b   // Division
+$_a % $_b   // Modulo
+$_a ** $_b  // Exponential
+?>
