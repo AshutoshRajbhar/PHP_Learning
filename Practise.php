776bcc4e464417f0464d22e9153e0430b62f0022
AshutoshRajbhar <ashutoshrajbhar@gmail.com>
2025-02-11 21:06:27 +0530

    Practise_Folder_Created

--- Practise.php
+++
@@ -1,8 +0,0 @@
-<!-- <?php 
-echo phpinfo();
-?>
- -->
-
- <?php 
-echo "'Tomorrow I\'ll learn PHP global variable.'"
- ?>
---
+++ Practise/Day_1_Practise.php
@@ -0,0 +1,12 @@
+<!-- 
+ 
+Write a PHP script to get the PHP version and configuration information. 
+
+-->
+
+
+<?php 
+echo phpinfo();
+?>
+
+
